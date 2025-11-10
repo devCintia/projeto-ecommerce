496305f69b709cbf457deb09e98a8981c768a350
Cintia <cintia.dev2024@gmail.com>
2025-11-09 21:50:04 -0300

    funcionalidade de abrir e fechar o menu hamburguer

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+
+// Função para abrir e fechar o menu hamburguer
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoMenu = document.querySelector('.menu-hamburguer');
+    const cabecalho = document.querySelector('.cabecalho');
+
+    botaoMenu.addEventListener('click', function () {
+        cabecalho.classList.toggle('menu-ativo');
+    });
+});
